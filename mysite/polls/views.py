21113e6f902ce3f859d707da2c0922ce7c4e5018
Leticia Siqueira <alvessiq@usc.edu>
2022-12-14 14:39:03 -0800

    feat(ICTRAINING-1362): Part 3
    raising a 404 errors
    
    [ICTRAINING-1366]

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,26 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 
+# t’s a very common idiom to load a template,
+# fill a context and return an HttpResponse object with the result of the rendered template.
+# Django provides a shortcut: render()
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
+# get_object_or_404 is a shortcut for try/except
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/vote.html', {'question': question})
 
