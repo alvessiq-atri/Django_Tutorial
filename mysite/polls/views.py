6f4f8f37ca99e6defe38eea6881c6cdd9e0f9c34
Leticia Siqueira <alvessiq@usc.edu>
2022-12-15 10:41:50 -0800

    test(ICTRAINING-1362): Part 5
    adding test and fixing bugs
    
    [ICTRAINING-1368]

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -37,8 +37,8 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
-            'question' : question,
-            'error_message' : "You didn't select a choice.",
+            'question': question,
+            'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.votes += 1
