297e611475215d189908379d5236b6f2b3dae5aa
Leticia Siqueira <alvessiq@usc.edu>
2022-12-14 16:29:25 -0800

    feat(ICTRAINING-1362): Part 4
    wrote a minimal form with submit button and redirection
    
    [ICTRAINING-1367]

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, Http404
-from django.template import loader
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Choice, Question
 
 
 # t’s a very common idiom to load a template,
@@ -26,5 +27,20 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/vote.html', {'question': question})
-
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        # This function helps avoid having to hardcode a URL in the view function.
+        # It is given the name of the view that we want to pass control to and
+        # the variable portion of the URL pattern that points to that view
