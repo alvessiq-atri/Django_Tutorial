6abf6d1ee267df22d4561d54c4e9180eddc546e8
Leticia Siqueira <alvessiq@usc.edu>
2022-12-15 11:06:54 -0800

    test(ICTRAINING-1362): Part 5
    adding another test for published recently
    
    [ICTRAINING-1368]

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -16,3 +16,13 @@ class QuestionModelTests(TestCase):
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions
+        whose pub_date is within the last day.
+        :return:
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
